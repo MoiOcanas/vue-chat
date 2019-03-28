6d72247b08e5eb99c9d12566e7e6f703dd18fa0d
MoiOcanas <alejandro.ocanas@outlook.com>
2019-03-28 10:53:20 -0500

    Adding sockets to component

--- routes/chat.js
+++ routes/chat.js
@@ -3,9 +3,24 @@ var router = express.Router();
 var mongoose = require('mongoose');
 var Chat = require('../models/Chat');
 
+
+//Server
 var app = express();
 var server = require('http').createServer(app);
 var io = require('socket.io')(server);
+
+server.listen(4000);
+
+io.on('connection', function(socket) {
+    console.log('User connected');
+    socket.io('disconnect', function() {
+        console.log('User disconnected');
+    });
+    socket.io('save-message', function(data) {
+        console.log(data);
+        io.emit('new-message', { message: data });
+    });
+})
 
 //Get all rooms
 router.get('/', function (req, res, next) {
